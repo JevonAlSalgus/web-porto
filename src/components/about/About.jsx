8035a1e20d48c7d6cbbfde2cfab1de1a071e75e3
JevonAlSalgus <jevonalsalgus@gmail.com>
2026-07-13 11:21:44 +0700

    Ganti CV

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Niclauss-Cv.pdf";
+import CV from "../../assets/CV_Jevon Al Salgus.pdf";
 import Info from "./Info";
 
 const About = () => {
